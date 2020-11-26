dda2d5ecc2f621f7e41641131653da7dbd9e77f3
keza1brenda <keza1brenda@gmail.com>
2020-11-26 11:35:14 +0200

    add first test to decryption class

--- src/test/java/DecryptionTest.java
+++ src/test/java/DecryptionTest.java
@@ -1,5 +1,12 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DecryptionTest {
 
+    @Test
+    public void firstEncode() throws Exception {
+        Encryption testEncodedMsg = new Encryption ("hello", 1);
+        assertEquals (true, testEncodedMsg instanceof Encryption);// this test test the overall process of decryption
+    }
 }
