f4f2f66546d64ddfc893c9e544f0e3471ecb13d3
keza1brenda <keza1brenda@gmail.com>
2020-11-26 12:13:32 +0200

    add codes to decryption class

--- src/main/java/Decryption.java
+++ src/main/java/Decryption.java
@@ -1,3 +1,37 @@
 public class Decryption {
+    private String msg;
+    private int key;
 
+    public Decryption(String msg, int key) {
+        this.msg = msg;
+        this.key = key;
+    }
+
+    public String getMsg() {
+        return this.msg;
+    }
+    public int getKey(){
+        return this.key;
+    }
+
+    public static String decryptedMsg(String msg, int key) {
+        String decryptMsg = "";
+        for (int i = 0; i < msg.length(); i++) {
+            if ((int) msg.charAt(i) == 32) {
+                decryptMsg += (char)32;
+            }
+            else if ((int) msg.charAt(i) - key < 97 && (int) msg.charAt(i) > 90) {
+                int temp = ((int) msg.charAt(i) - key) + 26;
+                decryptMsg += (char)temp;
+            }
+            else if ((int) msg.charAt(i) - key < 65) {
+                int temp = ((int) msg.charAt(i) - key) + 26;
+                decryptMsg+= (char)temp;
+            }
+            else {
+                decryptMsg += (char) ((int) msg.charAt(i) - key);
+            }
+        }
+        return decryptMsg;
+    }
 }
